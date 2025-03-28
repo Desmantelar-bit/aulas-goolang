a7dec6e454e0c308ee999ebf6c2099c7ac95ada7
Vinícius Morales <vinicius.morales@aluno.senai.br>
2025-03-28 08:19:06 -0300

    Operador Lógico

--- main.go
+++ main.go
@@ -4,9 +4,13 @@ import "fmt"
 
 func main(){
 	a, b := 10, 3
-	fmt.Println("A soma é:", a+b)
-	fmt.Println("A subtração é:", a-b)
-	fmt.Println("A multiplicação é:", a*b)
-	fmt.Println("A divisão é:", a/b, "e o resto é:", a%b)
-
+		fmt.Println("A soma é:", a+b)
+		fmt.Println("A subtração é:", a-b)
+		fmt.Println("A multiplicação é:", a*b)
+		fmt.Println("A divisão é:", a/b, "e o resto é:", a%b)
+	a++ // Pode-se fazer a++, a = a + 1 ou a += 1
+		fmt.Println("Incrementar valor à a:", a)
+	if a > 0 && b > 0 {
+		fmt.Println("A e B são positivos")
+	}
 }
