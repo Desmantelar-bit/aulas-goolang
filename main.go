2164d457c93ee66916d804d7c38546baaa8b2028
Vinícius Morales <vinicius.morales@aluno.senai.br>
2025-04-03 09:17:37 -0300

    Slice/Score/len/cap/reange

--- main.go
+++ main.go
@@ -9,5 +9,22 @@ func main() {
 	fmt.Println(nomes)
 	nomes[3] ="Clark Kent"
 	fmt.Println(nomes)
+	//Slice
+	var score = []int{100,200,300,400}
+	fmt.Println(score)
+	score[1] = 500
+	fmt.Println(score, len(score), cap(score))
+	rangeOne := score[1:3]
+	fmt.Println(rangeOne)
+	rangeTwo := score[2:]
+	fmt.Println(rangeTwo)
+	rangeThree := score[:3]
+	fmt.Println(rangeThree)
 
+	var superherois = []string{"Luffy", "Naruto", "Ichigo", "Goku"}
+	fmt.Println(superherois)
+	superherois = append(superherois, "Ben 10", "Superman", "Batman", "Flash")
+	fmt.Println(superherois)
+	fmt.Println(len(superherois), cap(superherois))
+	
 }
