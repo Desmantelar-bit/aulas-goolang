5f39e46722f18b429ddcdf8298a84915e05bdbcc
Vinícius Morales <vinicius.morales@aluno.senai.br>
2025-04-04 08:22:13 -0300

    Funções da Bibloteca Strings e Sort

--- main.go
+++ main.go
@@ -1,30 +1,23 @@
 package main
 
-import "fmt"
+import ("fmt"; "sort")
 
 func main() {
-	var ages = [4]int{15, 15, 16, 16}
-	nomes := [4]string{"Yan, Bruninha, Eduardinha, Pedrinha"}
-	fmt.Println(ages)
-	fmt.Println(nomes)
-	nomes[3] ="Clark Kent"
-	fmt.Println(nomes)
-	//Slice
-	var score = []int{100,200,300,400}
-	fmt.Println(score)
-	score[1] = 500
-	fmt.Println(score, len(score), cap(score))
-	rangeOne := score[1:3]
-	fmt.Println(rangeOne)
-	rangeTwo := score[2:]
-	fmt.Println(rangeTwo)
-	rangeThree := score[:3]
-	fmt.Println(rangeThree)
-
-	var superherois = []string{"Luffy", "Naruto", "Ichigo", "Goku"}
-	fmt.Println(superherois)
-	superherois = append(superherois, "Ben 10", "Superman", "Batman", "Flash")
-	fmt.Println(superherois)
-	fmt.Println(len(superherois), cap(superherois))
-	
+	/*greating := "Hello, Bia!"
+	fmt.Println(strings.Contains(greating, "Bia"))
+	fmt.Println(strings.ReplaceAll(greating, "Hello", "Hi")) 
+	fmt.Println(strings.ToUpper(greating))
+	fmt.Println(strings.Index(greating, "Bia"))
+	fmt.Println(strings.Split(greating, "Hello"))
+	*/
+	ages := []int{50,80, 20, 100, 40}
+	sort.Ints(ages)
+	fmt.Println(ages)	
+	index := sort.SearchInts(ages, 50)
+	fmt.Println(index)
+	names := []string{"Bia","Sabrina", "Larissa"}
+	sort.Strings(names)
+	fmt.Println(names)
+	index = sort.SearchStrings(names, "Sabrina")
+	fmt.Println(index)
 }
