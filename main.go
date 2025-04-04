e4b57b1dbea9afdfd7567a564db8caff2e69470e
Vinícius Morales <vinicius.morales@aluno.senai.br>
2025-04-04 09:41:46 -0300

    For em Vetores

--- main.go
+++ main.go
@@ -1,6 +1,6 @@
 package main
 
-import ("fmt"; "sort")
+import ("fmt")
 
 func main() {
 	/*greating := "Hello, Bia!"
@@ -9,15 +9,53 @@ func main() {
 	fmt.Println(strings.ToUpper(greating))
 	fmt.Println(strings.Index(greating, "Bia"))
 	fmt.Println(strings.Split(greating, "Hello"))
-	*/
-	ages := []int{50,80, 20, 100, 40}
 	sort.Ints(ages)
 	fmt.Println(ages)	
 	index := sort.SearchInts(ages, 50)
 	fmt.Println(index)
-	names := []string{"Bia","Sabrina", "Larissa"}
 	sort.Strings(names)
 	fmt.Println(names)
 	index = sort.SearchStrings(names, "Sabrina")
-	fmt.Println(index)
+	fmt.Println(index)*/
+	
+	/*ages := []int{50,80, 20, 100, 40}
+	names := []string{"Bia","Sabrina", "Larissa"}
+	x := 0
+	for x< 5 {
+		fmt.Println(x)
+		x++
+	}
+	for i:= 0; i < 5; i++ {
+		fmt.Println("for 2:", i)
+	}
+
+	for i:= 0; i <len(names); i++ {
+		fmt.Println("for 3:", names[i])
+	}
+
+	for index, value := range names {
+		fmt.Println("O índice é:", index,"e o valor é:", value)
+	}
+
+	for index, value := range ages {
+		fmt.Println("O índice é:", index,"e o valor é:", value)
+		ages = append(ages, 200)
+		fmt.Println("O tamanho do vetor é:",len(ages), "A capacidade do vetor é:",cap(ages))
+
+	}*/
+
+	superherois := []string{"Superman", "Batman", "Flash"}
+
+	for  index, value := range superherois {
+			fmt.Println("O superheroi é:", value, "E sua posição é:", index)
+			if value == "Batman" {
+				fmt.Println("Gotham precisa de mim!")
+			}
+			if value == "Flash" {
+				fmt.Println("Eu sou a velocidade!")
+			}
+			if value == "Superman" {
+				fmt.Println("Eu sou um deus!")
+			}
+	}
 }
