2d0c065c75b3a78e0e52a0721c44372413e1b38e
Vinícius Morales <vinicius.morales@aluno.senai.br>
2025-04-10 09:37:47 -0300

    Estrutura de decisão if

--- main.go
+++ main.go
@@ -3,59 +3,31 @@ package main
 import ("fmt")
 
 func main() {
-	/*greating := "Hello, Bia!"
-	fmt.Println(strings.Contains(greating, "Bia"))
-	fmt.Println(strings.ReplaceAll(greating, "Hello", "Hi")) 
-	fmt.Println(strings.ToUpper(greating))
-	fmt.Println(strings.Index(greating, "Bia"))
-	fmt.Println(strings.Split(greating, "Hello"))
-	sort.Ints(ages)
-	fmt.Println(ages)	
-	index := sort.SearchInts(ages, 50)
-	fmt.Println(index)
-	sort.Strings(names)
-	fmt.Println(names)
-	index = sort.SearchStrings(names, "Sabrina")
-	fmt.Println(index)*/
-	
-	/*ages := []int{50,80, 20, 100, 40}
-	names := []string{"Bia","Sabrina", "Larissa"}
-	x := 0
-	for x< 5 {
-		fmt.Println(x)
-		x++
-	}
-	for i:= 0; i < 5; i++ {
-		fmt.Println("for 2:", i)
-	}
+	/*e := 45
+	fmt.Println(age <= 50)
+	fmt.Println(age >= 50)
+	fmt.Println(age == 50)
+	fmt.Println(age != 50)
 
-	for i:= 0; i <len(names); i++ {
-		fmt.Println("for 3:", names[i])
-	}
-
-	for index, value := range names {
-		fmt.Println("O índice é:", index,"e o valor é:", value)
-	}
-
-	for index, value := range ages {
-		fmt.Println("O índice é:", index,"e o valor é:", value)
-		ages = append(ages, 200)
-		fmt.Println("O tamanho do vetor é:",len(ages), "A capacidade do vetor é:",cap(ages))
-
+	if age < 30 {
+		fmt.Println("Menor que 30 anos")
+	} else if age < 40 {
+		fmt.Println("Menor que 40 anos")
+	} else {
+		fmt.Println("Não é menor que 40 anos")
 	}*/
 
-	superherois := []string{"Superman", "Batman", "Flash"}
+names := []string{"Eduardo", "Pedro L.", "Bruno", "Yan", "Arthur", "Sabrina"}
 
-	for  index, value := range superherois {
-			fmt.Println("O superheroi é:", value, "E sua posição é:", index)
-			if value == "Batman" {
-				fmt.Println("Gotham precisa de mim!")
-			}
-			if value == "Flash" {
-				fmt.Println("Eu sou a velocidade!")
-			}
-			if value == "Superman" {
-				fmt.Println("Eu sou um deus!")
-			}
+for index, value := range names{
+	if index == 1{
+		fmt.Println("Continua após a posição", index, " e valor", value)
+		continue
 	}
+	if index >2{
+		fmt.Println("sair após", index)
+		break
+	}
+	fmt.Println("Valor: ", value)
+}
 }
