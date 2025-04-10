adb011e8ab6bdb6117f4d5437531bf6f0f63811f
Vinícius Morales <vinicius.morales@aluno.senai.br>
2025-04-10 10:21:50 -0300

    Atividade em sala

--- main.go
+++ main.go
@@ -3,31 +3,14 @@ package main
 import ("fmt")
 
 func main() {
-	/*e := 45
-	fmt.Println(age <= 50)
-	fmt.Println(age >= 50)
-	fmt.Println(age == 50)
-	fmt.Println(age != 50)
-
-	if age < 30 {
-		fmt.Println("Menor que 30 anos")
-	} else if age < 40 {
-		fmt.Println("Menor que 40 anos")
-	} else {
-		fmt.Println("Não é menor que 40 anos")
-	}*/
-
-names := []string{"Eduardo", "Pedro L.", "Bruno", "Yan", "Arthur", "Sabrina"}
-
-for index, value := range names{
-	if index == 1{
-		fmt.Println("Continua após a posição", index, " e valor", value)
-		continue
+	var age int
+	fmt.Println("Digite a sua idade:")
+	fmt.Scan(&age)
+	if age <=18 {
+		fmt.Println("Você é menor idade.")
+	} else if age >=18 && age <= 60 {
+		fmt.Println("Você é adulto.")
+	} else if age > 60 {
+		fmt.Println("Você é idoso.")
 	}
-	if index >2{
-		fmt.Println("sair após", index)
-		break
-	}
-	fmt.Println("Valor: ", value)
-}
 }
