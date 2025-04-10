4420f1d3440a888de4398ce6ad10adccfb21ea28
Vinícius Morales <vinicius.morales@aluno.senai.br>
2025-04-10 11:06:13 -0300

    Exercício 2 - Banco (Saldo e depósito)

--- main.go
+++ main.go
@@ -3,14 +3,50 @@ package main
 import ("fmt")
 
 func main() {
-	var age int
-	fmt.Println("Digite a sua idade:")
-	fmt.Scan(&age)
-	if age <=18 {
-		fmt.Println("Você é menor idade.")
-	} else if age >=18 && age <= 60 {
-		fmt.Println("Você é adulto.")
-	} else if age > 60 {
-		fmt.Println("Você é idoso.")
-	}
+	var saldo float64 
+	var opcao int
+	var depósito float64
+	var saque float64
+	var resultado float64
+	fmt.Println("Bem-vindo ao banco!")
+	fmt.Println("Digite o valor inicial do saldo:")
+	fmt.Scan(&saldo)
+	fmt.Println("O que você deseja fazer?")
+	fmt.Println("1 - Depositar")
+	fmt.Println("2 - Sacar")
+	fmt.Println("3 - Ver saldo")
+	fmt.Scan(&opcao)
+	switch opcao {
+	case 1:
+		fmt.Println("Digite o valor do depósito:")
+		fmt.Scan(&depósito)
+			if depósito <= 0{
+				fmt.Println("Valor inválido!")
+			} else {
+				resultado = saldo + depósito
+				fmt.Print("Depósito de ", depósito," realizado com sucesso! Saldo atual: ",resultado)
+			}
+		break
+	case 2:
+		fmt.Println("Digite o valor do saque:")
+		fmt.Scanln(&saque)
+			if saque > saldo {
+				fmt.Println("Saldo insuficiente!")
+			} else if saque <= 0 {
+				fmt.Println("Valor inválido!")
+			}else{
+				resultado = saldo - saque
+				fmt.Print("Saque de ", saque," realizado com sucesso! Saldo atual: ",resultado)
+			}
+		break
+	case 3:
+		fmt.Print("Seu saldo é: ",saldo)
+		break
+	default:
+		fmt.Println("Opção inválida!")
+		return
+
+}
+	fmt.Println("Obrigado por usar nosso banco!")
+	fmt.Println("Até logo! Qualquer problema entre em contato com o suporte!")
 }
