3d8e877f3b2612624fba570e44203aff6efbfff9
Vinícius Morales <vinicius.morales@aluno.senai.br>
2025-04-11 13:27:14 -0300

    Função em GO

--- main.go
+++ main.go
@@ -2,55 +2,15 @@ package main
 
 import ("fmt")
 
-func main() {
-	var saldo float64 
-	var opcao int
-	var depósito float64
-	var saque float64
-	var resultado float64
-	fmt.Println("Bem-vindo ao banco!")
-	fmt.Println("Digite o valor inicial do saldo:")
-	fmt.Scan(&saldo)
-	fmt.Println("O que você deseja fazer?")
-	fmt.Println("1 - Depositar")
-	fmt.Println("2 - Sacar")
-	fmt.Println("3 - Ver saldo")
-	fmt.Scan(&opcao)
-	switch opcao {
-	case 1:
-		fmt.Println("Digite o valor do depósito:")
-		fmt.Scan(&depósito)
-			if depósito <= 0{
-				fmt.Println("Valor inválido!")
-				main ()
-			} else {
-				resultado = saldo + depósito
-				fmt.Print("Depósito de ", depósito," realizado com sucesso! Saldo atual: ",resultado)
-			}
-		break
-	case 2:
-		fmt.Println("Digite o valor do saque:")
-		fmt.Scan(&saque)
-			if saque > saldo {
-				fmt.Println("Saldo insuficiente!")
-				main ()
-			} else if saque <= 0 {
-				fmt.Println("Valor inválido!")
-				main ()
-			}else{
-				resultado = saldo - saque
-				fmt.Print("Saque de ", saque," realizado com sucesso! Saldo atual: ",resultado)
-			}
-		break
-	case 3:
-		fmt.Print("Seu saldo é: ",saldo)
-		break
-	default:
-		fmt.Println("Opção inválida!")
-		fmt.Println("Escolha uma opção válida! Tente novamente.")
-		main ()
-
+func sayGreeeting(nome string) {
+	fmt.Println("Hello,", nome)
 }
-	fmt.Println("Obrigado por usar nosso banco!")
-	fmt.Println("Até logo! Qualquer problema entre em contato com o suporte!")
+func addNumber(numero1 int, numero2 int) int {
+	return numero1 + numero2
+}
+
+func main() {
+	sayGreeeting("Juvelino")
+	resultado := addNumber(10, 20)
+	fmt.Println("Resultado:", resultado)
 }
