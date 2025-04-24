e2d90761fb8ca9aa96fef3583d08a1d07ba24372
Vinícius Morales <vinicius.morales@aluno.senai.br>
2025-04-24 13:24:22 -0300

    Retomada de função com mais de um retorno

--- main.go
+++ main.go
@@ -2,35 +2,11 @@ package main
 
 import ("fmt")
 
-func dividir(dividendo float64, divisor float64) (float64, string){
-	if divisor == 0 || dividendo == 0 {
-		return 0, "Error: Division by zero"
-	} else{
-	result := dividendo / divisor
-	return result, ""
-	}	
+func pegarNome()(string, string){
+	return "Barry", "Allen"
 }
 
-// A função dividir recebe dois números e retorna o resultado da divisão e uma string de erro, se houver.
-// A função retorna um número float64 e uma string.
-
-func operacaoBasica(a int, b int) (int, int, int){
-	soma:= a + b
-	subtracao:= a - b
-	multiplicacao:= a * b
-	return soma, subtracao, multiplicacao
-}
-// A função operacaoBasica recebe dois números inteiros e retorna a soma, subtração e multiplicação deles.
-// A função retorna três valores inteiros.
-
-func main() { 
-	resultado, erro := dividir(10, 2)
-	if erro != "" {
-		fmt.Println(erro)
-	} else {
-		fmt.Println("Resultado:", resultado,erro)
-	}
-
-	soma, subtracao, multiplicacao := operacaoBasica(10, 2)
-	fmt.Println("Soma:", soma, "Subtração:", subtracao, "Multiplicação:", multiplicacao)
+func main() {
+	nome, sobrenome := pegarNome()
+	fmt.Println(nome, sobrenome)
 }
